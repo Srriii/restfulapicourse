52e6baeaf7cc30fbb3bce3c99206b45160afd356
Sri Hartati <cheezy90@hotmail.com>
2019-07-17 14:22:44 +0800

    workshop02

--- workshop02/citiesdb.js
+++ workshop02/citiesdb.js
@@ -1,3 +1,4 @@
+const uuidv1 = require('uuid/v1')
 const MongoClient = require('mongodb').MongoClient;
 
 const f = function(config) {
@@ -12,8 +13,8 @@ f.prototype.getDB = function() {
 	);
 }
 f.prototype.findCitiesByState = function(state, params = {}) {
-	const limit = parseInt(params['limit']) || 10;
-	const offset = parseInt(params['offset']) || 0;
+	const limit = parseInt(params.['limit']) || 10;
+	const offset = parseInt(params.['offset']) || 0;
 	return (
 		this.getDB()
 			.then(db => 
@@ -86,6 +87,7 @@ f.prototype.findAllStates = function() {
 }
 
 f.prototype.insertCity = function(params) {
+	params._id = uuidv1().substring(0, 8);
 	return (
 		this.getDB()
 			.then(db => 
